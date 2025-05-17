da9628ec72775ff868a371b54eb28f589ce82e20
durki <durki>
2025-05-17 21:29:12 +0200

    moved backend source

--- push-backend-container.sh
+++ push-backend-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 az acr login --name databaseregistry
-docker build ./backend/ -t backend:latest
+docker build ./backend-spring/ -t backend:latest
 docker tah backend databaseregistry.azurecr.io/backend
 docker push databaseregistry.azurecr.io/backend
